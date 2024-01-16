272918b78dda2380bc062b99f1479a05fb482d89
Micki Balder <mfbalder@gmail.com>
2024-01-15 20:39:01 -0700

    Add comment about nil manager ID issue

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,7 @@
 class Employee < ApplicationRecord
     belongs_to :manager
 
+    #TODO: Get this unless to work, fails if the manager ID is nil
     validates_presence_of :manager, unless: Proc.new { |e| e.manager_id == nil }
 
 end
