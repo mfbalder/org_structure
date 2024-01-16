d31d6266dba512fff93a11ed397b34430242806d
Micki Balder <mfbalder@gmail.com>
2024-01-15 20:04:27 -0700

    Add associations and validation to models (model validation condition WIP)

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
-    
+    belongs_to :manager
+
+    validates_presence_of :manager, unless: Proc.new { |e| e.manager_id == nil }
+
 end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,3 @@
 class Manager < Employee
+    has_many :employees
 end
