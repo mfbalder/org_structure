949cc036737e3a01474bc2e561a60f520e90c939
Micki Balder <mfbalder@gmail.com>
2024-01-17 16:51:49 -0700

    Create employee endpoints

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,42 +1,42 @@
+# frozen_string_literal: true
+
 class EmployeesController < ApplicationController
-    def create
-        employee = Employee.create!(employee_params)
-        render json: employee
-    end
+  def create
+    employee = Employee.create!(employee_params)
+    render json: employee
+  end
 
-    def update
-        employee = Employee.find_by(id: params[:id])
-        employee.update!(employee_params)
+  def update
+    employee = Employee.find_by(id: params[:id])
+    employee.update!(employee_params)
 
-        render json: employee
-    end
+    render json: employee
+  end
 
-    def show
-        employee = Employee.find_by(id: params[:id])
-        render json: employee
-    end
+  def show
+    employee = Employee.find_by(id: params[:id])
+    render json: employee
+  end
 
-    def index
-        if params[:hire_date]
-            employees = Employee.where(hire_date: params[:hire_data])
-        else
-            employees = Employee.all
-        end
+  def index
+    employees = if params[:hire_date]
+                  Employee.where(hire_date: params[:hire_data])
+                else
+                  Employee.all
+                end
 
-        render json: employees
-    end
+    render json: employees
+  end
 
-    def employees
-        employees = Manager.find_by(id: params[:employee_id]).employees
+  def employees
+    employees = Manager.find_by(id: params[:employee_id]).employees
 
-        render json: employees
-    end
+    render json: employees
+  end
 
-    private
+  private
 
-    def employee_params
-        params.require(:employee).permit(:name, :date_of_birth, :date_of_hire, :manager_id, :type, :bonus)
-    end
-
-
+  def employee_params
+    params.require(:employee).permit(:name, :date_of_birth, :date_of_hire, :manager_id, :type, :bonus)
+  end
 end
